989157511edc2c0f470f2f497e9684f54ae1804e
Pranay Venkat Aluri <145622254+helloGithub326@users.noreply.github.com>
2026-06-14 22:53:55 -0500

    Finished C++ documentation

--- src/wpilib-python-documentation.tsx
+++ src/wpilib-python-documentation.tsx
@@ -2,4 +2,4 @@ import { List, Cache, ActionPanel, Action } from "@raycast/api";
 import { useState, useRef } from "react";
 
 const cache = new Cache();
-const SEARCH_TEXT_KEY = "searchText";
+const SEARCH_TEXT_KEY = "searchTextPython";
